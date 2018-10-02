dc02dd27daf82de56ac61f237198668e93ebe561
Jared R. Mallas <jrtytn@gmail.com>
2018-10-01 19:57:41 -0500

    main class javadoc

--- src/org/jrm/GarageEntry.java
+++ src/org/jrm/GarageEntry.java
@@ -3,6 +3,11 @@ package org.jrm;
 import org.jrm.data.garage.Garage;
 import org.jrm.pos.POSEntry;
 
+/**
+ * Class model for a Garage entry system
+ * @author Jared Mallas
+ * @version 1.0
+ */
 public class GarageEntry
 {
     public static void main(String[] args)
--- src/org/jrm/GarageExit.java
+++ src/org/jrm/GarageExit.java
@@ -3,6 +3,11 @@ package org.jrm;
 import org.jrm.data.garage.Garage;
 import org.jrm.pos.POSExit;
 
+/**
+ * Class model for a garage exit system
+ * @author Jared Mallas
+ * @version 1.0
+ */
 public class GarageExit
 {
     public static void main(String[] args)
